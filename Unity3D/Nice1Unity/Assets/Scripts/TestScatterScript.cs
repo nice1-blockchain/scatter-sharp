ce3a5085f6c1ce55516670919fe60353de944e31
Alberto R <alberto@3in-tech.com>
2021-04-11 11:59:52 +0200

    getAccount

--- Unity3D/Nice1Unity/Assets/Scripts/TestScatterScript.cs
+++ Unity3D/Nice1Unity/Assets/Scripts/TestScatterScript.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using EosSharp.Core.Api.v1;
 using EosSharp.Core.Exceptions;
+using System.Threading.Tasks;
 
 public class TestScatterScript : MonoBehaviour
 {
@@ -100,4 +101,66 @@ public class TestScatterScript : MonoBehaviour
         var scatterUnitTests = new ScatterUnitTests(this);
         await scatterUnitTests.TestAll();
     }
+
+    public async Task<IdentityAccount> GetAccount()
+    {
+        try
+        {
+            ScatterSharp.Core.Api.Network network = new ScatterSharp.Core.Api.Network()
+            {
+                blockchain = ScatterConstants.Blockchains.EOSIO,
+                host = "jungle3.cryptolions.io",
+                port = 443,
+                chainId = "2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840"
+            };
+
+            var fileStorage = new FileStorageProvider(Application.persistentDataPath + "/scatterapp.dat");
+            using (var scatter = new Scatter(new ScatterConfigurator()
+            {
+                AppName = "UNITY-PC-SCATTER",
+                Network = network,
+                StorageProvider = fileStorage
+            }, this))
+            {
+                await scatter.Connect();
+
+                await scatter.GetIdentity(new IdentityRequiredFields()
+                {
+                    accounts = new List<ScatterSharp.Core.Api.Network>()
+                {
+                    network
+                },
+                    location = new List<LocationFields>(),
+                    personal = new List<PersonalFields>()
+                });
+
+                var eos = new Eos(new EosSharp.Core.EosConfigurator()
+                {
+                    ChainId = network.chainId,
+                    HttpEndpoint = network.GetHttpEndpoint(),
+                    SignProvider = new ScatterSignatureProvider(scatter)
+                });
+
+                var account = scatter.Identity.accounts.First();
+
+                return account;
+            }
+        }
+        catch (ApiErrorException ex)
+        {
+            print(JsonConvert.SerializeObject(ex.error));
+            return null;
+        }
+        catch (ApiException ex)
+        {
+            print(ex.Content);
+            return null;
+        }
+        catch (Exception ex)
+        {
+            print(JsonConvert.SerializeObject(ex));
+            return null;
+        }
+    }
+
 }
